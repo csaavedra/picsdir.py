503f2ef38661e2b14d617fef363696a8f8ea5036
Claudio Saavedra <csaavedra@igalia.com>
2022-08-16 13:35:28 +0300

    Update to latest version I am currently using
    Use at your own risk, I am only publishing it in github because
    I don't want to have copies lying around in different computers.

--- picsdir.py
+++ picsdir.py
@@ -26,23 +26,28 @@ import mimetypes
 
 def move_smartly(file, dir_in, dir_out, suffix):
     image_file = os.path.join (dir_in, file)
-    image = pyexiv2.Image(image_file)
-    image.readMetadata()
-    date_creation = image['Exif.Image.DateTime']
-    str_date = date_creation.strftime('%Y-%m-%d-')
+    metadata = pyexiv2.ImageMetadata(image_file)
+    metadata.read()
+    if ('Exif.Photo.DateTimeOriginal' in metadata):
+        date_creation = metadata['Exif.Photo.DateTimeOriginal'].value
+        print(date_creation)
+        str_date = date_creation.strftime('%Y-%m-%d-')
+        target_dir = os.path.join (dir_out, str_date + suffix)
 
-    target_dir = os.path.join (dir_out, str_date + suffix)
+        if (not os.path.exists(target_dir)):
+            os.mkdir(target_dir)
 
-    if (not os.path.exists(target_dir)):
-        print "creating dir " + target_dir
-        os.mkdir(target_dir)
-
-    shutil.move (image_file, target_dir)
+        try:
+            shutil.move (image_file, target_dir)
+        except shutil.Error:
+            print file + " already exists in destination, not moving."
+    else:
+        print file + " has no date metadata, not moving."
 
 
 def Main(args):
     if (len (args) < 2):
-        sys.exit("not enough parameters")
+        sys.exit("picsdir.py input_dir [output_dir [suffix]]")
     dir_in = args[1]
     if (len (args) < 3):
         dir_out = dir_in
@@ -52,13 +57,15 @@ def Main(args):
         suffix = ''
     else:
         suffix = args[3]
-
+    mimetypes.knownfiles.append('/home/claudio/.mime.types')
+    mimetypes.init()
     for root, dirs, files in os.walk (dir_in):
         for f in files:
             filename = os.path.join (root, f)
-            if mimetypes.guess_type(filename)[0] == 'image/jpeg':
+            if mimetypes.guess_type(filename)[0] in ['image/jpeg', 'image/x-canon-cr2']:
+		print filename
                 move_smartly (filename, dir_in, dir_out, suffix)
+    print sum(os.path.getsize(join(root, name)) for name in files)
 
 start=Main(sys.argv)
 
-print sum(getsize(join(root, name)) for name in files)
